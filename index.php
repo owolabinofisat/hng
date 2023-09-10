8b56b9999d7098641d3aff85a8de25fe390dc759
owolabinofisat <133150394+owolabinofisat@users.noreply.github.com>
2023-09-10 07:12:56 -0700

    Add files via upload
    First Assignment corrected

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+// Get the values of the GET parameters
+$slackName = $_GET['slack_name'] ?? '';
+$track = $_GET['track'] ?? '';
+
+// Validate the 'track' parameter (you can add more validation if needed)
+$validTracks = ['Web Development', 'Data Science', 'Mobile App Development'];
+if (!in_array($track, $validTracks)) {
+    echo json_encode(['error' => 'Invalid track parameter.']);
+    exit;
+}
+
+// Get the current day of the week
+$currentDay = date('l');
+
+// Get the current UTC time
+$currentUtcTime = gmdate('Y-m-d H:i:s', time());
+
+// Validate the time within +/-2 hours
+$currentTime = strtotime($currentUtcTime);
+$twoHoursAgo = strtotime('-2 hours');
+$twoHoursLater = strtotime('+2 hours');
+
+if ($currentTime < $twoHoursAgo || $currentTime > $twoHoursLater) {
+    echo json_encode(['error' => 'Current time is not within +/-2 hours of UTC time.']);
+    exit;
+}
+
+// Define GitHub URLs (replace with actual URLs)
+$fileUrl = 'https://github.com/owolabinofisat/repo/hng-.git';
+$sourceCodeUrl = 'https://github.com/owolabinofisat';
+
+// Prepare the response
+$response = [
+    'slack_name' => $slackName,
+    'day' => $currentDay,
+    'current_utc_time' => $currentUtcTime,
+    'track' => $track,
+    'file_url' => $fileUrl,
+    'source_code_url' => $sourceCodeUrl,
+    'status_code' => 'success',
+];
+
+// Set the content type to JSON
+header('Content-Type: application/json');
+
+// Output the JSON response
+echo json_encode($response);
